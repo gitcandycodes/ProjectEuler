799924b988f91a2f76734fd3cf89521d3cc90610
Shimakee <shimakee@gmail.com>
2020-07-01 16:56:56 +0800

    Exercies 1 - finding multiples of 3 & 5.

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -10,12 +10,38 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            FizzBuzz(1000);
 
+            Console.WriteLine("Find multiples of 3 or 5 below what number?."); //ask for input
+            string input = Console.ReadLine(); //store input
+            int maxNumber;
+            Int32.TryParse(input, out maxNumber); //convert to int
+
+
+            int sum = FindMultiples(maxNumber); //execute method
+            //FizzBuzz(maxNumber); //execute fizzbuz
+
+            Console.WriteLine($"{sum} is the sum.");
             Console.ReadLine();
         }
 
-        private static void FizzBuzz(int maxNumber)
+        private static int FindMultiples(int maxNumber) //finding the multiples
+        {
+
+            int sum = 0;
+
+            for (int i = 0; i < maxNumber; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    Console.WriteLine($"{i} is a multiple of 3 or 5.");
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        private static void FizzBuzz(int maxNumber) //doing fizzbuzz
         {
             for (int i = 0; i < maxNumber; i++)
             {
