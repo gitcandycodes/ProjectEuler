805e75f3933df7f3aa6eb0ca8d2797d20ac75ddf
Shimakee <shimakee@gmail.com>
2020-07-01 16:44:03 +0800

    Add project files.

---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz(1000);
+
+            Console.ReadLine();
+        }
+
+        private static void FizzBuzz(int maxNumber)
+        {
+            for (int i = 0; i < maxNumber; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+            }
+        }
+    }
+}
